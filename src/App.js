11a2423509268ec6542d0ec556fb12e83b441340
Dimitar.Atanasov <dimitar.atanasov@dimitar.atanasov-mbp>
2020-03-31 13:58:20 +0300

    added shop page, item collection data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
+import ShopScreen from "./screens/shop/ShopScreen";
 import "./styles/app.css";
 
 function App() {
   return (
-    <>
-      <HomeScreen />
-    </>
+    <Switch>
+      <Route exact path="/" component={HomeScreen} />
+      <Route path="/shop" component={ShopScreen} />
+    </Switch>
   );
 }
 
---
+++ src/screens/shop/ShopScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import ColectionPreview from "../../components/preview/ColectionPreview";
+import { SHOP_DATA } from "../../data/shop.data";
+
+class ShopScreen extends Component {
+  state = {
+    collections: SHOP_DATA
+  };
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...restCollectionItems }) => (
+          <ColectionPreview key={id} {...restCollectionItems} />
+        ))}
+      </div>
+    );
+  }
+}
+export default ShopScreen;
